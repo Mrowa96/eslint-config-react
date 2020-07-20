ef029957323e28723ad747cb1e110ebd404b146a
Paweł Mrowiec <work@pawel-mrowiec.pl>
2020-07-20 19:23:43 +0200

    Update dependencies, allow for console and change jsx-a11y/label-has-associated-control

--- base.js
+++ base.js
@@ -6,7 +6,14 @@ module.exports = {
   },
   rules: {
     'no-prototype-builtins': 'off',
+    'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
     'prettier/prettier': 'error',
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        assert: 'either',
+      },
+    ],
   },
   env: {
     node: true,
--- typescript.js
+++ typescript.js
@@ -1,8 +1,9 @@
+const baseConfig = require('./base');
+
 module.exports = {
   extends: [
     'airbnb-typescript',
     'airbnb/hooks',
-    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:import/typescript',
@@ -19,6 +20,7 @@ module.exports = {
     project: './tsconfig.json',
   },
   rules: {
+    ...baseConfig.rules,
     'import/prefer-default-export': 'off',
     'react/jsx-props-no-spreading': 'off',
     'react-hooks/exhaustive-deps': 'off',
